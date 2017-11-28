02387bece5126cf164edfb0f38099497410676c9
Nikola Irinchev <irinchev@me.com>
2017-11-28 15:16:46 +0100

    Add schema introspection

--- src/service.ts
+++ src/service.ts
@@ -26,6 +26,7 @@ import {
 import * as GraphQLHTTP from 'express-graphql';
 
 import { DocumentNode, GraphQLError, GraphQLSchema, GraphQLObjectType, buildSchema } from 'graphql';
+import { ObjectSchemaProperty } from 'realm';
 
 function getSchemaFromRealmByObjectName(realm: Realm, objectName: string): Realm.ObjectSchema | undefined {
     return realm.schema.find(s => s.name === objectName)
@@ -67,10 +68,87 @@ export class GraphQLService {
 
     async getSchema(path: string) : Promise<GraphQLSchema> {
         let realm = await this.server.openRealm(path)
-        return buildSchema(`
-            type Query {
-                hello: String
+
+        let schema = '';
+        let types = new Array<string>();
+
+        for (const obj of realm.schema) {
+            schema += `type ${obj.name} {\n`;
+
+            for (const key in obj.properties) {
+                if (obj.properties.hasOwnProperty(key)) {
+                    const prop = obj.properties[key] as ObjectSchemaProperty;
+                    if (prop.type === 'linkingObjects') {
+                        continue;
+                    }
+                    let type = '';
+                    switch (prop.type) {
+                        case 'object':
+                            type = prop.objectType;
+                            break;
+                        case 'list':
+                            let innerType = this.getTypeString(prop.objectType, prop.optional);
+                            if (innerType === null) {
+                                innerType = prop.objectType;
+                            }
+                            type = `[${innerType}]`;
+                            break;
+                        default:
+                            type = this.getTypeString(prop.type, prop.optional);
+                            break;
+                    }
+
+                    schema += `    ${key}: ${type}\n`
+
+                }
             }
-        `);
+
+            types.push(obj.name);
+          
+            schema += '}\n\n';
+        }
+
+        schema += 'type Query {\n';
+
+        for (const type of types) {
+            schema += `    ${this.camelcase(type)}: [${type}!]\n`
+        }
+
+        schema += '}';
+
+        return buildSchema(schema);
+    }
+
+    getTypeString(prop: string, optional: Boolean): string {
+        let result = '';
+        switch (prop) {
+            case 'bool':
+                result = 'Boolean';
+                break;
+            case 'int':
+            case 'date':
+                result = 'Int';
+                break;
+            case 'float':
+            case 'double':
+                result = 'Float';
+                break;
+            case 'string':
+            case 'data':
+                result = 'String';
+                break;
+            default:
+                return null;
+        }
+
+        if (!optional) {
+            result += '!';
+        }
+
+        return result;
+    }
+
+    camelcase(value: string) : string {
+        return value.charAt(0).toLowerCase() + value.slice(1);
     }
 }
