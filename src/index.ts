607f55a017219e6b9b10f9c28a8767b220a6c9b0
Maximilian Alexander <mbalex99@gmail.com>
2017-12-11 23:26:55 -0800

    moving service to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,3 @@
 /* tslint:disable:no-console */
 
-import * as path from 'path';
-import { BasicServer } from 'realm-object-server';
-import { GraphQLService } from './service';
-
-const server = new BasicServer();
-
-server.addService(new GraphQLService({
-  schemaCacheSettings: {},
-  disableAuthentication: true
-}));
-
-server
-  .start({
-    dataPath: path.join(__dirname, '../data'),
-  })
-  .then(() => {
-    console.log(`Realm Object Server was started on ${server.address}`);
-  })
-  .catch((err) => {
-    console.error(`Error starting Realm Object Server: ${err.message}`);
-  });
+export * from './service';
